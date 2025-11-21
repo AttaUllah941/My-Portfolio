7538a3e25f521a4f4abb10edb09c19de519bd4bb
Atta Ullah <atta.ullah@clarisync.com>
2025-11-21 12:17:33 +0500

    my portfolio

---
+++ src/config/api.config.js
@@ -0,0 +1,11 @@
+// API Configuration
+export const apiConfig = {
+  apiUrl: "http://172.16.10.42/client-backend-tool/public/api/v1",
+  qpApiUrl: 'http://172.16.10.42/client-backend-quantum-project/public/cbre-client/api',
+  qpApiUrlForPortalStatus: 'http://172.16.10.42/client-backend-tool/public/api',
+  mediaUrl: "https://kyndryl-dev.s3.us-east-2.amazonaws.com/", // for images, videos, documents, etc....
+  fileManagerApiURL: 'https://kyndryl-staging-api.horizoncenter.co/cbre-client/api',
+};
+
+export default apiConfig;
+
